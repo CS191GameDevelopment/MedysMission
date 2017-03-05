44a3b0ce25656c55c7a0e6613b6bfedb2d3f0b2c
Christian Dave Baclayon <davebaclayon@gmail.com>
2017-03-05 08:37:22 +0800

    With sprites

---
+++ Medy's Mission Final Project/Assets/Scripts/MedyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MedyController : MonoBehaviour {
+	public float moveRate = 0.2f;
+	public bool isDead = false;
+
+	public Transform fireProjectile;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(false == isDead){
+			shoot ();
+			movePlayer ();
+		}
+	}
+
+	public void shoot(){
+		if(Input.GetKeyDown("space")){
+			Instantiate (fireProjectile, new Vector2(transform.position.x, transform.position.y)
+				, fireProjectile.rotation);
+		}
+	}
+
+	public void movePlayer(){
+		float moveX = Input.GetAxis("Horizontal");
+		float moveY = Input.GetAxis("Vertical");
+
+		//Move the player
+		transform.position = new Vector2(transform.position.x + moveX * moveRate
+			, transform.position.y + moveY * moveRate);
+	}
+}
---
+++ Medy's Mission Final Project/Assets/Scripts/TabletController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabletController : MonoBehaviour {
+
+	public float moveRate = .1f;
+
+	void Start(){
+		
+	}
+
+	void FixedUpdate(){
+		transform.position = new Vector2 (transform.position.x, transform.position.y + moveRate);
+	}
+
+	void OnCollisionEnter2D(Collision2D target){
+		if(target.gameObject.tag == "TopBorder"){
+			Destroy (gameObject);
+		}else if(target.gameObject.tag == "Enemy"){
+			Destroy (gameObject);
+			Destroy (target.gameObject);
+		}
+	}
+}
