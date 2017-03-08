4ac1a47d8a57992ee1d63e4dc32f7777fcae07f2
Christian Dave Baclayon <davebaclayon@gmail.com>
2017-03-09 06:09:39 +0800

    This is the latest build

---
+++ Medy's Mission Final Project/Assets/Scripts/RulesScreenScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RulesScreenScript : MonoBehaviour {
+
+
+	public float moveRate =0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2 (transform.position.x, transform.position.y + moveRate);
+
+
+		if (transform.position.y > 10) {
+			transform.position = new Vector2 (transform.position.x, -6);
+		}
+	}
+}
